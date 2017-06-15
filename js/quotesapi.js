729a42b18f3e1ebe15f03c31d2463148cc093ca9
Giridhar Subramanian <giri.psbb@gmail.com>
2017-06-15 09:09:03 +0530

    added quote to window

--- js/quotesapi.js
+++ js/quotesapi.js
@@ -1,32 +1,38 @@
-window.quoteGen = {
-	getQuote : function(callback){
-		// $.ajax({
-		// 	type:'GET',
-		// 	url: 'https://favqs.com/api/qotd',
-		// 	success: function(response){
-		// 		console.log(response);
-		// 	}
-		// });
-		this.getJSON(callback);
-	},
+// window.quoteGen = {
+// 	getQuote : function(callback){
+// 		// $.ajax({
+// 		// 	type:'GET',
+// 		// 	url: 'https://favqs.com/api/qotd',
+// 		// 	success: function(response){
+// 		// 		console.log(response);
+// 		// 	}
+// 		// });
+// 		this.getJSON(callback);
+// 	},
 
-	getJSON : function(callback){
-		$.ajax({
-			type:'GET',
-			url: 'https://favqs.com/api/qotd',
-			success: function(response){
-				console.log(response);
-			}
-		});
-	}	
-};
-
-// $(function(){
-// 	$.ajax({
+// 	getJSON : function(callback){
+// 		$.ajax({
 // 			type:'GET',
 // 			url: 'https://favqs.com/api/qotd',
 // 			success: function(response){
+// 				$resp = response;
 // 				console.log(response);
+				
 // 			}
 // 		});
-// });
+// 	}	
+// };
+
+$(function(){
+	$.ajax({
+			type:'GET',
+			url: 'https://favqs.com/api/qotd',
+			success: function(response){
+				console.log(response['quote'].body);
+				console.log(response['quote'].author);
+				$quote = response['quote'].body;
+				$author = response['quote'].author;
+				$('#quote').append('<p>'+$quote+'</p><br>'+$author);
+			}
+		});
+});
