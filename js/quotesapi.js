0b48714da1476ccaf968532545831efd63d6a22d
Giridhar Subramanian <giri.psbb@gmail.com>
2017-06-15 21:56:16 +0530

    added css

--- js/quotesapi.js
+++ js/quotesapi.js
@@ -24,6 +24,17 @@
 // };
 
 $(function(){
+	$.ajax({
+			type:'GET',
+			url: 'https://favqs.com/api/qotd',
+			success: function(response){
+				console.log(response['quote'].body);
+				console.log(response['quote'].author);
+				$quote = response['quote'].body;
+				$author = response['quote'].author;
+				$('#quote').html('<p>'+$quote+'</p><br>'+$author);
+			}
+		});
 	$('#quoteGen').on('click',function(){
 		$.ajax({
 			type:'GET',
